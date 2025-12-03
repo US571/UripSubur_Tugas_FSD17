9f43cc631ad46d775b9671386df421ee2cb498a5
UripSubur <urip.sbr@gmail.com>
2025-12-03 11:25:45 +0700

    add data

---
+++ app.js
@@ -0,0 +1,23 @@
+//coremodule
+//file.system
+const fs = require("fs");
+
+//Menuliskan string ke File (Synchronous)
+fs.writeFileSync("tes.txt","Hello World secara Synchronous!")
+// fs.readFile("tes.txt","utf-8",(err,data)=> {
+// if (err) throw err;
+// console.log(data);
+// });
+
+const readline = require("readline");
+
+const rl = readline.createInterface({
+input: process.stdin,
+output: process.stdout
+});
+
+rl.question("What is your name? ", (name) => {
+console.log(`Thank you ${name}`);
+
+rl.close();
+});
--- tesnode.js
+++ tesnode.js
@@ -1,6 +1,6 @@
 console.info("tesnode");
 console.info("BIODATA");
-console.info("Nama   : Urip Subur");
-console.info("TTL    : 30 Agustus 1980");
+console.info("Nama  : Urip Subur");
+console.info("TTL   : Pekalongan, 30 Agustus 1980");
 console.info("Hobi  : Renang, Membaca");
 console.info("Alamat: Desa Siwalan - Kec. Siwalan - Pekalongan");
